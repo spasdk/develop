e24c58192fc19635050a468b7adc049a1c92b964
Stanislav Kalashnik <sk@infomir.eu>
2016-01-28 16:53:53 +0200

    correct livereload handling

--- static.js
+++ static.js
@@ -8,15 +8,24 @@
 
 'use strict';
 
-var tag    = require('spa-dom').tag/*,
-    config = require('../../config/static')*/;
+//var tag = require('spa-dom').tag;
 
+
+//window.LiveReloadOptions = {port: LIVERELOAD.port};
+window.LiveReloadOptions = {
+    host: location.hostname,
+    port: LIVERELOAD.port || 35729
+};
+//console.log(require('spa-gulp-livereload/config').default.tinylr);
+//console.log(LIVERELOAD);
+
+require('livereload-js/dist/livereload.js');
 
 // livereload activation
 //if ( config.livereload ) {
     // load external script
-document.head.appendChild(tag('script', {
-    type: 'text/javascript',
-    src: '/node_modules/livereload-js/dist/livereload.js?host=' + location.hostname + '&port=' + LIVERELOAD.port
-}));
+//document.head.appendChild(tag('script', {
+//    type: 'text/javascript',
+//    src: '/node_modules/livereload-js/dist/livereload.js?host=' + location.hostname + '&port=' + LIVERELOAD.port
+//}));
 //}
